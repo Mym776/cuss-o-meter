a9c574b134c629016e798e0d68c2492c2f691df2
BereketGirma <bereketgirma002@gmail.com>
2025-06-14 09:42:15 -0500

    Initial commit

---
+++ bot/main.py
@@ -0,0 +1,16 @@
+from pyrogram import Client, filters
+from config import BOT_TOKEN, API_HASH, API_ID
+
+
+app = Client("Cuss-o-meter", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+
+@app.on_message(filters.private & filters.command("start"))
+def private_chat_handler(client, message):
+    message.reply_text("Hello! I'm Cuss-o-meter. The greatest cussing meter bot.")
+    
+@app.on_message(filters.command("start") & filters.group)
+def group_start_handle(client, message):
+    message.reply_text("Hello! I'm Cuss-o-meter. The greatest cussing meter bot.")
+        
+if __name__ == "__main__":
+    app.run()
