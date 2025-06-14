43bb88af3067748c51919fb9f4e1f96137d3abc3
Mym776 <Mymhs7@gmail.com>
2025-06-14 16:15:32 +0300

    tokenize

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 from config import BOT_TOKEN, API_HASH, API_ID
 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 
 app = Client("Cuss-o-meter", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
@@ -16,6 +18,27 @@ def group_start_handle(client, message):
 @app.on_message(filters.private & filters.command("cuss"))
 def private_chat_handler(client, message):
     message.reply_text("that aint funny cuhh")
+
+# trail 2 
+@app.on_message()
+def private_chat_handler(client, message):
+    stop_words = set(stopwords.words('english'))
+    tokenized = sent_tokenize(message)
+    for i in tokenized:
+    
+    # Word tokenizers is used to find the words 
+    # and punctuation in a string
+    wordsList = nltk.word_tokenize(i)
+
+    # removing stop words from wordList
+    wordsList = [w for w in wordsList if not w in stop_words] 
+
+    #  Using a Tagger. Which is part-of-speech 
+    # tagger or POS-tagger. 
+    tagged = nltk.pos_tag(wordsList)
+
+    print(tagged)
+
       
 if __name__ == "__main__":
     app.run()
