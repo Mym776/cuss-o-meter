b77f82b732898c473de47e0eb5023f1b13246aaa
Mym776 <Mymhs7@gmail.com>
2025-06-15 16:57:32 +0300

    firebase connection successful

--- bot/backend/db.py
+++ bot/backend/db.py
@@ -6,7 +6,8 @@ from firebase_admin import firestore
 from pyrogram import Client, filters
 from config import BOT_TOKEN, API_HASH, API_ID, FIREBASE_KEY
 
-def dbConnect():
+# connects with the db and gets the firestore client
+def dbInitialization():
     if not FIREBASE_KEY:
         print("Error: FIREBASE_KEY environment variable not set!")
         exit()
@@ -14,7 +15,8 @@ def dbConnect():
         cred = credentials.Certificate(FIREBASE_KEY)
         firebase_admin.initialize_app(cred)
         print("Firebase Admin SDK initialized successfully!")
-        #db = firestore.client()
+        db = firestore.client()
+        return db
 
 
 
@@ -23,3 +25,17 @@ def dbConnect():
         print(f"Error initializing Firebase Admin SDK: {e}")
         
         exit()
+
+
+# pings with db by trying to get a nonexistent collection
+def dbPing(firestore_client):
+    try:
+       
+        doc_ref = firestore_client.collection("health_checks").document("ping_test_document")
+        doc = doc_ref.get()
+
+        print("Firestore 'ping' successful: Connection established.")
+        return True
+    except Exception as e:
+        print(f"Firestore 'ping' failed: {e}")
+        return False
